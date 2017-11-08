b7f5efb364b1d0d8d1faa414d384705131eb1d47
Jason Shortino <akagopher8@gmail.com>
2017-11-07 21:16:37 -0800

    Added initial code for the deck and card classes.

--- Blackjack/src/mygame/Card.java
+++ Blackjack/src/mygame/Card.java
@@ -1,11 +1,102 @@
 package mygame;
 
 public class Card{
-    private String Face;
-    private String Type;
+    private char face;
+    private char suit;
     private int value;
     
-    public void Card(){
-        
+    //default constructor
+    public Card(){
+        face = '2';
+        suit = 'S';
+        value = 2;
+    }
+    
+    //overridden constructor
+    public Card(char f, char s){
+        face = f;
+        suit = s;
+        value = calculateValue(face);
+    }
+    
+    //get the value of face
+    public char getFace(){
+        return face;
+    }
+    
+    //get the value of suit
+    public char getSuit(){
+        return suit;
+    }
+    
+    //get the value of the card
+    public int getValue(){
+        return value;
+    }
+    
+    //set the face of the card and update the value
+    public void setFace(char f){
+        face = f;
+        value = calculateValue(f);
+    }
+    
+    //set the card's suit
+    public void setSuit(char s){
+        suit = s;
+    }
+    
+    //set the value of the card
+    public void setValue(int v){
+        value = v;
+    }
+    
+    //calculate the value of the card based on the face
+    //T = 10, J = Jack, Q = Queen, K = King, A = High Ace, a = Low Ace
+    private int calculateValue(char face){
+        int val = 0;
+        switch (face){
+            case '2':
+                 val = 2;
+                 break;
+            case '3':
+                 val = 3;
+                 break;
+            case '4':
+                 val = 4;
+                 break;
+            case '5':
+                 val = 5;
+                 break;
+            case '6':
+                 val = 6;
+                 break;
+            case '7':
+                 val = 7;
+                 break;
+            case '8':
+                 val = 8;
+                 break;
+            case '9':
+                 val = 9;
+                 break;
+            case 'T':
+                 val = 10;
+                 break;
+            case 'J':
+                 val = 10;
+                 break;
+            case 'Q':
+                 val = 10;
+                 break;
+            case 'K':
+                 val = 10;
+                 break;
+            case 'A':
+                 val = 11;
+                 break;
+            default:
+                val = 1;
+        }
+        return val;
     }
 }
